3921374fdba0b60f47e913a7c594d23197f1d8d6
0xC0FFE2 <github1@ncloud.sbs>
2025-05-09 12:24:14 +0900

    feat : (#4) - 전체 엔티티 세팅

--- src/main/java/team/weero/app/persistence/notice/entity/Notice.java
+++ src/main/java/team/weero/app/persistence/notice/entity/Notice.java
@@ -1,22 +1,27 @@
-package team.weero.app.persistence.user.entity;
+package team.weero.app.persistence.notice.entity;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
+import team.weero.app.persistence.user.entity.User;
 
 import java.util.UUID;
 
 @Entity
-@Table(name = "tbl_user")
-public class User {
+@Table(name = "tbl_notice")
+public class Notice {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
-    @Column(nullable = false, length = 255)
-    private String password;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @Column(nullable = false, unique = true, length = 255)
-    private String email;
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String contents;
 }
