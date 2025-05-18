ff70f03914609cd499addeeb0b8cd0efc7f22b93
박지민 <pjmlove2008@gmail.com>
2025-05-18 22:26:31 +0900

    feat: (#3) - 로그인, 회원가입 dto

---
+++ src/main/java/team/weero/app/core/auth/dto/request/LoginRequest.java
@@ -0,0 +1,16 @@
+package team.weero.app.core.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequest(
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "유효한 이메일 형식이어야 합니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 8, max = 255, message = "비밀번호는 8자 이상 입력해야 합니다.")
+        String password
+) {}
---
+++ src/main/java/team/weero/app/core/auth/dto/request/SignupRequest.java
@@ -0,0 +1,25 @@
+package team.weero.app.core.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+
+        @NotBlank(message = "사용자 이름은 필수입니다.")
+        @Size(min = 1, max = 20, message = "사용자 이름은 1자 이상 20자 이하로 입력해야 합니다.")
+        String username,
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 8, max = 255, message = "비밀번호는 8자 이상 입력해야 합니다.")
+        String password,
+
+        @NotBlank(message = "비밀번호 확인은 필수입니다.")
+        @Size(min = 8, max = 255, message = "비밀번호 확인은 8자 이상 입력해야 합니다.")
+        String checkPassword,
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "유효한 이메일 형식이어야 합니다.")
+        String email
+
+) {}
