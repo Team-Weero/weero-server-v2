6006940af3586144d2d0ad46e8f2fbffee13e7b8
박지민 <pjmlove2008@gmail.com>
2025-06-01 21:46:49 +0900

    feat: (#3) - 비밀번호 확인 삭제 및 redis 추가

--- src/main/java/team/weero/app/persistence/auth/repository/RefreshTokenRepository.java
+++ src/main/java/team/weero/app/persistence/auth/repository/RefreshTokenRepository.java
@@ -1,9 +1,7 @@
 package team.weero.app.persistence.auth.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import team.weero.app.persistence.auth.entity.RefreshToken;
 
-@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
