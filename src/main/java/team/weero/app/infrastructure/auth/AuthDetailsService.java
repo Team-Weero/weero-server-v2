6b0b9f13e9f9b85dfe74f8b28fb55a27ebca49a4
박지민 <pjmlove2008@gmail.com>
2025-06-02 12:12:45 +0900

    refactor: (#3) -  User 엔티티에서 중복 accountId 필드 제거

--- src/main/java/team/weero/app/infrastructure/auth/AuthDetailsService.java
+++ src/main/java/team/weero/app/infrastructure/auth/AuthDetailsService.java
@@ -5,18 +5,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import team.weero.app.persistence.user.repository.UserRepository;
+import team.weero.app.core.auth.spi.CommandAuthPort;
+import team.weero.app.persistence.student.entity.Student;
+import team.weero.app.persistence.user.entity.User;
 
 @Service
 @RequiredArgsConstructor
 public class AuthDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
-
+    private final CommandAuthPort commandAuthPort;
     @Override
     public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
-        return userRepository.findByAccountId(accountId)
-                .map(AuthDetails::new)
+        Student student = commandAuthPort.findByAccountId(accountId)
                 .orElseThrow(() -> new UsernameNotFoundException(accountId));
+
+        User user = student.getUser();
+
+        return new AuthDetails(user, student);
     }
 }
