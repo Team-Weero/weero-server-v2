b3fd584670ab45712aa359776aaf835291704c1d
박지민 <pjmlove2008@gmail.com>
2025-07-02 17:06:37 +0900

    feat: (#10) - CommandCounselingServiceImpl 추가

---
+++ src/main/java/team/weero/app/core/counseling/service/CommandCounselingServiceImpl.java
@@ -0,0 +1,42 @@
+package team.weero.app.core.counseling.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.weero.app.core.counseling.dto.request.CounselingRequest;
+import team.weero.app.core.counseling.exception.DuplicateReservationException;
+import team.weero.app.core.counseling.spi.CommandCounselingPort;
+import team.weero.app.persistence.counseling.entity.CounselingApplication;
+import team.weero.app.persistence.student.entity.Student;
+import team.weero.app.persistence.teacher.entity.Teacher;
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class CommandCounselingServiceImpl implements CommandCounselingService {
+
+    private final GetCounselingService getCounselingService;
+    private final CommandCounselingPort commandCounselingPort;
+
+    @Transactional
+    public void applyForCounseling(CounselingRequest request) {
+
+        Student student = getCounselingService.findByStudent(request);
+        Teacher teacher = getCounselingService.findByTeacher(request);
+
+        boolean exists = getCounselingService.existsByTeacherAndDateAndTime(request);
+        if (exists) {
+            throw new DuplicateReservationException();
+        }
+
+        CounselingApplication counseling = new CounselingApplication();
+        counseling.setStudent(student);
+        counseling.setTeacher(teacher);
+        counseling.setCounselDate(request.date());
+        counseling.setTime(request.time());
+        counseling.setApplicationDate(LocalDate.now());
+        counseling.setChecked(false);
+
+        commandCounselingPort.save(counseling);
+    }
+}
