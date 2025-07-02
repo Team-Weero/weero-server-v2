25fcf4b3ba9853c9e8f0a1b4cde3cf944427a8ce
박지민 <pjmlove2008@gmail.com>
2025-07-02 17:12:40 +0900

    feat: (#10) - 에러코드 추가

--- src/main/java/team/weero/app/infrastructure/error/exception/ErrorCode.java
+++ src/main/java/team/weero/app/infrastructure/error/exception/ErrorCode.java
@@ -28,6 +28,10 @@ public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
 
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+
+    DUPLICATE_RESERVATION(HttpStatus.CONFLICT, "Duplicate Reservation");
+
     private final HttpStatus status;
     private final String message;
 }
