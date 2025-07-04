852a7ac102a0b8f706bed529304b4619f9d6cf8d
박지민 <pjmlove2008@gmail.com>
2025-07-04 13:36:00 +0900

    refactor: (#10) - CommandCounselingPort 수정

--- src/main/java/team/weero/app/core/counseling/spi/CommandCounselingPort.java
+++ src/main/java/team/weero/app/core/counseling/spi/CommandCounselingPort.java
@@ -1,6 +1,5 @@
 package team.weero.app.core.counseling.spi;
 
-import team.weero.app.core.counseling.dto.request.CounselingRequest;
 import team.weero.app.persistence.counseling.entity.CounselingApplication;
 
 public interface CommandCounselingPort {
