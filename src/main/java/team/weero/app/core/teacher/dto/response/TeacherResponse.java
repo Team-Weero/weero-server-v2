c6047830c5327dcf416b63439c857efb2f2e7172
박지민 <pjmlove2008@gmail.com>
2025-08-01 16:06:11 +0900

    feat: (#18) - deviceTokens 타입을 String에서 DeviceToken 엔티티로 변경

--- src/main/java/team/weero/app/core/teacher/dto/response/TeacherResponse.java
+++ src/main/java/team/weero/app/core/teacher/dto/response/TeacherResponse.java
@@ -1,12 +1,15 @@
 package team.weero.app.core.teacher.dto.response;
 
+import team.weero.app.infrastructure.firebase.entity.DeviceToken;
+
+import java.util.List;
 import java.util.UUID;
 
 public record TeacherResponse(
     UUID id,
     String name,
     String accountId,
-    String deviceToken,
+    DeviceToken deviceTokens,
     String noNotificationStartTime,
     String noNotificationEndTime
 ) {
