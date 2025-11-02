07439867e1a38f491d74910088a541c45fca1de3
박지민 <pjmlove2008@gmail.com>
2025-11-02 21:58:25 +0900

    feat: (#25) - teacher yml 환경변수 세팅 및 Properties 파일 추가

---
+++ src/main/java/team/weero/app/infrastructure/config/AdminProperties.java
@@ -0,0 +1,18 @@
+package team.weero.app.infrastructure.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "spring.admin.teacher")
+public class AdminProperties {
+
+    private final String id;
+    private final String password;
+    private final String name;
+    private final String deviceToken;
+    private final String notificationStartTime;
+    private final String notificationEndTime;
+}
