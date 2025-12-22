9df7c9414de84c7ae38b8f5b05a22de7b2c942d3
Lee Donghyun <d@donghyun.cc>
2025-12-22 16:23:59 +0900

    feat : User, Student Use Case 및 DTO 추가

---
+++ src/main/java/team/weero/app/application/student/dto/response/StudentResponse.java
@@ -0,0 +1,34 @@
+package team.weero.app.application.student.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import team.weero.app.domain.student.model.Student;
+import team.weero.app.domain.student.model.StudentRole;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class StudentResponse {
+    private UUID id;
+    private String name;
+    private String nickname;
+    private String accountId;
+    private String gcn;
+    private StudentRole role;
+    private UUID userId;
+
+    public static StudentResponse from(Student student) {
+        return StudentResponse.builder()
+                .id(student.getId())
+                .name(student.getName())
+                .nickname(student.getNickname())
+                .accountId(student.getAccountId())
+                .gcn(student.getGcn())
+                .role(student.getRole())
+                .userId(student.getUserId())
+                .build();
+    }
+}
