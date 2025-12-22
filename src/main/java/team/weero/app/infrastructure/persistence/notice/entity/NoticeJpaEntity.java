0b84d4903464e746cf5686052cd1e1437f9e3d00
Lee Donghyun <d@donghyun.cc>
2025-12-22 16:24:03 +0900

    feat : Notice JPA Entity 및 Repository 구현체 추가

---
+++ src/main/java/team/weero/app/infrastructure/persistence/notice/entity/NoticeJpaEntity.java
@@ -0,0 +1,32 @@
+package team.weero.app.infrastructure.persistence.notice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import team.weero.app.infrastructure.persistence.user.entity.UserJpaEntity;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "tbl_notice")
+public class NoticeJpaEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserJpaEntity user;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String contents;
+}
