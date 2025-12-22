87a97031bac2351da5ce4c937c44696755708ba9
Lee Donghyun <d@donghyun.cc>
2025-12-22 16:23:58 +0900

    feat : Concern 도메인 모델 및 레포지토리 추가

---
+++ src/main/java/team/weero/app/domain/concern/exception/ConcernNotFoundException.java
@@ -0,0 +1,12 @@
+package team.weero.app.domain.concern.exception;
+
+import team.weero.app.infrastructure.error.exception.ErrorCode;
+import team.weero.app.infrastructure.error.exception.WeeRoException;
+
+public class ConcernNotFoundException extends WeeRoException {
+    public static final WeeRoException EXCEPTION = new ConcernNotFoundException();
+
+    private ConcernNotFoundException() {
+        super(ErrorCode.CONCERN_NOT_FOUND);
+    }
+}
