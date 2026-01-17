fee1bf7c79a47d3167eff8baad160714330f0fbe
Lee Donghyun <d@donghyun.cc>
2026-01-17 22:16:16 +0900

    feat: 리프레시 토큰 어댑터 및 엔티티 구현

---
+++ src/main/java/team/weero/app/adapter/out/persistence/RefreshTokenAdapter.java
@@ -0,0 +1,33 @@
+package team.weero.app.adapter.out.persistence;
+
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import team.weero.app.adapter.out.auth.entity.RefreshTokenJpaEntity;
+import team.weero.app.adapter.out.auth.repository.RefreshTokenRedisRepository;
+import team.weero.app.application.port.out.LoadRefreshTokenPort;
+import team.weero.app.application.port.out.SaveRefreshTokenPort;
+import team.weero.app.domain.auth.RefreshToken;
+
+@Component
+@RequiredArgsConstructor
+public class RefreshTokenAdapter implements SaveRefreshTokenPort, LoadRefreshTokenPort {
+
+  private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+  @Override
+  public void save(RefreshToken refreshToken) {
+    RefreshTokenJpaEntity entity = RefreshTokenJpaEntity.from(refreshToken);
+    refreshTokenRedisRepository.save(entity);
+  }
+
+  @Override
+  public Optional<RefreshToken> loadByToken(String token) {
+    return refreshTokenRedisRepository.findByToken(token).map(RefreshTokenJpaEntity::toDomain);
+  }
+
+  @Override
+  public void deleteByToken(String token) {
+    refreshTokenRedisRepository.deleteByToken(token);
+  }
+}
