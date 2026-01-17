a368ed67079a488e8edecbdaa97f6b8843a1a7d7
Lee Donghyun <d@donghyun.cc>
2026-01-17 22:16:30 +0900

    feat: WeeRoApplication에 @EnableJpaAuditing 및 @EnableRedisRepositories 추가

--- src/main/java/team/weero/app/WeeRoApplication.java
+++ src/main/java/team/weero/app/WeeRoApplication.java
@@ -3,9 +3,12 @@ package team.weero.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import team.weero.app.global.security.jwt.JwtProperties;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableConfigurationProperties(JwtProperties.class)
 public class WeeRoApplication {
 
   public static void main(String[] args) {
