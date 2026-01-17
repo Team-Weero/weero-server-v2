1fe502b1afae47811c9cf318c5e0ece20de864c4
Lee Donghyun <d@donghyun.cc>
2026-01-17 22:15:40 +0900

    style: Status와 StudentRole enum 들여쓰기 통일

--- src/main/java/team/weero/app/domain/counsel/type/Status.java
+++ src/main/java/team/weero/app/domain/counsel/type/Status.java
@@ -1,8 +1,8 @@
 package team.weero.app.domain.counsel.type;
 
 public enum Status {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED,
+  CANCELLED
 }
