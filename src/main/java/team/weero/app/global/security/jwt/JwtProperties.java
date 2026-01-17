d0774b0febd9feca0530b3ea868200381f8d4319
Lee Donghyun <d@donghyun.cc>
2026-01-17 22:16:20 +0900

    feat: Spring Security 설정 및 JWT 속성 추가

---
+++ src/main/java/team/weero/app/global/security/jwt/JwtProperties.java
@@ -0,0 +1,51 @@
+package team.weero.app.global.security.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.jwt")
+public class JwtProperties {
+
+  private final String secretKey;
+  private final int accessExp;
+  private final int refreshExp;
+  private final String header;
+  private final String prefix;
+  private final SecretKey key;
+
+  public JwtProperties(
+      String secretKey, int accessExp, int refreshExp, String header, String prefix) {
+    this.secretKey = secretKey;
+    this.accessExp = accessExp;
+    this.refreshExp = refreshExp;
+    this.header = header;
+    this.prefix = prefix;
+    this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+  }
+
+  public String getSecretKey() {
+    return secretKey;
+  }
+
+  public int getAccessExp() {
+    return accessExp;
+  }
+
+  public int getRefreshExp() {
+    return refreshExp;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getPrefix() {
+    return prefix;
+  }
+
+  public SecretKey getKey() {
+    return key;
+  }
+}
