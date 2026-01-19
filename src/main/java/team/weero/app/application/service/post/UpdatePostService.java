9b8c81c9c41f4f1d796d84c521acb7bb5b552590
박지민 <pjmlove2008@gmail.com>
2026-01-19 19:18:53 +0900

    style(#44): 스타일 포맷팅 적용

--- src/main/java/team/weero/app/application/service/post/UpdatePostService.java
+++ src/main/java/team/weero/app/application/service/post/UpdatePostService.java
@@ -20,8 +20,7 @@ public class UpdatePostService implements UpdatePostUseCase {
 
   @Override
   public void execute(UUID postId, UUID userId, UpdatePostRequest request) {
-    loadStudentPort.loadByUserId(userId)
-            .orElseThrow(StudentNotFoundException::new);
+    loadStudentPort.loadByUserId(userId).orElseThrow(StudentNotFoundException::new);
 
     updatePostPort.update(postId, userId, request.title(), request.content());
   }
