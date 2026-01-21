dad76c30ad8b8491600f1dafc1543e03ae1cb4d9
박지민 <pjmlove2008@gmail.com>
2026-01-21 20:28:24 +0900

    feat(#47): 양방향 매핑 추가

--- src/main/java/team/weero/app/adapter/out/user/entity/UserJpaEntity.java
+++ src/main/java/team/weero/app/adapter/out/user/entity/UserJpaEntity.java
@@ -1,13 +1,14 @@
 package team.weero.app.adapter.out.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.weero.app.adapter.out.student.entity.StudentJpaEntity;
+import team.weero.app.adapter.out.teacher.entity.TeacherJpaEntity;
 import team.weero.app.global.entity.BaseTimeEntity;
 
 @Entity
@@ -23,6 +24,14 @@ public class UserJpaEntity extends BaseTimeEntity {
 
   @Column(nullable = false, columnDefinition = "VARCHAR(255)")
   private String password;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "student_id", nullable = false)
+  private StudentJpaEntity student;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "teacher_id", nullable = false)
+  private TeacherJpaEntity teacher;
 
   private LocalDateTime deletedAt;
 
