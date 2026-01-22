4c0aef26382216296770df9d82c7ceffaae47f4b
박지민 <pjmlove2008@gmail.com>
2026-01-22 11:28:40 +0900

    feat(#47): id 반환하도록 수정

--- src/main/java/team/weero/app/adapter/in/web/answer/dto/response/GetAnswerResponse.java
+++ src/main/java/team/weero/app/adapter/in/web/answer/dto/response/GetAnswerResponse.java
@@ -1,10 +1,12 @@
 package team.weero.app.adapter.in.web.answer.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
 import team.weero.app.domain.answer.model.Answer;
 
-public record GetAnswerResponse(String Answer, String nickName, LocalDateTime createdAt) {
+public record GetAnswerResponse(UUID id, String Answer, String nickName, LocalDateTime createdAt) {
   public static GetAnswerResponse from(Answer answer) {
-    return new GetAnswerResponse(answer.getAnswer(), answer.getNickName(), answer.getCreatedAt());
+    return new GetAnswerResponse(answer.getId() , answer.getAnswer(), answer.getNickName(), answer.getCreatedAt());
   }
 }
