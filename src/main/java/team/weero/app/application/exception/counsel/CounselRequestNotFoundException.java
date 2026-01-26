2b89dc81485b9761ab701c17b81d76fdef1e6e00
Lee Donghyun <d@donghyun.cc>
2026-01-26 09:32:22 +0900

    feat(#49): CounselRequestNotFoundException 추가

---
+++ src/main/java/team/weero/app/application/exception/counsel/CounselRequestNotFoundException.java
@@ -0,0 +1,10 @@
+package team.weero.app.application.exception.counsel;
+
+import team.weero.app.application.exception.counsel.error.CounselRequestErrorCode;
+import team.weero.app.global.error.exception.WeeRoException;
+
+public class CounselRequestNotFoundException extends WeeRoException {
+    public CounselRequestNotFoundException() {
+        super(CounselRequestErrorCode.COUNSEL_REQUEST_NOT_FOUND);
+    }
+}
