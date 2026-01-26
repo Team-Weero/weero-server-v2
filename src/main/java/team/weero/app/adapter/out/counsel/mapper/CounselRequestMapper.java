4770ed2c0c985c7c131049e376c04c5da952edd8
Lee Donghyun <d@donghyun.cc>
2026-01-26 09:33:11 +0900

    feat(#49): CounselRequestMapper 구현

---
+++ src/main/java/team/weero/app/adapter/out/counsel/mapper/CounselRequestMapper.java
@@ -0,0 +1,44 @@
+package team.weero.app.adapter.out.counsel.mapper;
+
+import org.springframework.stereotype.Component;
+import team.weero.app.adapter.out.counsel.entity.CounselRequestJpaEntity;
+import team.weero.app.adapter.out.student.entity.StudentJpaEntity;
+import team.weero.app.adapter.out.teacher.entity.TeacherJpaEntity;
+import team.weero.app.domain.counsel.CounselRequest;
+
+@Component
+public class CounselRequestMapper {
+
+    public CounselRequest toDomain(CounselRequestJpaEntity entity) {
+        return CounselRequest.builder()
+                .id(entity.getId())
+                .accessPassword(entity.getAccessPassword())
+                .status(entity.getStatus())
+                .gender(entity.getGender())
+                .hasCounselingExperience(entity.isHasCounselingExperience())
+                .category(entity.getCategory())
+                .studentId(entity.getStudent().getId())
+                .studentName(entity.getStudent().getName())
+                .teacherId(entity.getTeacher().getId())
+                .teacherName(entity.getTeacher().getName())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .deletedTime(entity.getDeletedTime())
+                .build();
+    }
+
+    public static CounselRequestJpaEntity toEntity(
+            CounselRequest counselRequest,
+            StudentJpaEntity student,
+            TeacherJpaEntity teacher) {
+        return CounselRequestJpaEntity.builder()
+                .accessPassword(counselRequest.getAccessPassword())
+                .status(counselRequest.getStatus())
+                .gender(counselRequest.getGender())
+                .hasCounselingExperience(counselRequest.isHasCounselingExperience())
+                .category(counselRequest.getCategory())
+                .student(student)
+                .teacher(teacher)
+                .build();
+    }
+}
