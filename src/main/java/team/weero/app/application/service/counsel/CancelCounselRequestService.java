5e357f93193de830295c915baf9a77ffe0b8e2dd
Lee Donghyun <d@donghyun.cc>
2026-01-26 09:33:00 +0900

    feat(#49): CancelCounselRequestService 구현

---
+++ src/main/java/team/weero/app/application/service/counsel/CancelCounselRequestService.java
@@ -0,0 +1,43 @@
+package team.weero.app.application.service.counsel;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.weero.app.adapter.in.web.student.dto.response.StudentInfo;
+import team.weero.app.application.exception.counsel.CounselRequestNotFoundException;
+import team.weero.app.application.exception.counsel.InvalidCounselRequestStatusException;
+import team.weero.app.application.exception.student.StudentNotFoundException;
+import team.weero.app.application.port.in.counsel.CancelCounselRequestUseCase;
+import team.weero.app.application.port.out.counsel.CheckCounselRequestOwnerPort;
+import team.weero.app.application.port.out.counsel.DeleteCounselRequestPort;
+import team.weero.app.application.port.out.counsel.LoadCounselRequestPort;
+import team.weero.app.application.port.out.student.LoadStudentPort;
+import team.weero.app.domain.counsel.CounselRequest;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CancelCounselRequestService implements CancelCounselRequestUseCase {
+
+    private final LoadCounselRequestPort loadCounselRequestPort;
+    private final DeleteCounselRequestPort deleteCounselRequestPort;
+    private final CheckCounselRequestOwnerPort checkCounselRequestOwnerPort;
+    private final LoadStudentPort loadStudentPort;
+
+    @Override
+    public void execute(UUID id, UUID userId) {
+        StudentInfo studentInfo =
+                loadStudentPort.loadByUserId(userId).orElseThrow(StudentNotFoundException::new);
+
+        CounselRequest existing =
+                loadCounselRequestPort.loadById(id).orElseThrow(CounselRequestNotFoundException::new);
+
+        if (!existing.isPending()) {
+            throw new InvalidCounselRequestStatusException();
+        }
+
+        deleteCounselRequestPort.softDelete(id, userId);
+    }
+}
