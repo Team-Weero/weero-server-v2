3263c76302768d7da576aecf13863fd7945bd82c
Lee Donghyun <d@donghyun.cc>
2026-01-26 09:33:29 +0900

    feat(#49): 스케줄링 활성화 설정 추가

--- src/main/java/team/weero/app/WeeRoApplication.java
+++ src/main/java/team/weero/app/WeeRoApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import team.weero.app.global.security.jwt.JwtProperties;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableConfigurationProperties(JwtProperties.class)
+@EnableScheduling
 public class WeeRoApplication {
 
   public static void main(String[] args) {
