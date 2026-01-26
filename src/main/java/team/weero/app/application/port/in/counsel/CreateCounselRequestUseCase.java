243af6867217ff8d95ea6645aeb17859cb1ac612
Lee Donghyun <d@donghyun.cc>
2026-01-26 09:32:45 +0900

    feat(#49): CreateCounselRequestUseCase 인터페이스 정의

---
+++ src/main/java/team/weero/app/application/port/in/counsel/CreateCounselRequestUseCase.java
@@ -0,0 +1,10 @@
+package team.weero.app.application.port.in.counsel;
+
+import team.weero.app.adapter.in.web.counsel.dto.request.CreateCounselRequestRequest;
+import team.weero.app.adapter.in.web.counsel.dto.response.CounselRequestResponse;
+
+import java.util.UUID;
+
+public interface CreateCounselRequestUseCase {
+    CounselRequestResponse execute(CreateCounselRequestRequest request, UUID studentId);
+}
