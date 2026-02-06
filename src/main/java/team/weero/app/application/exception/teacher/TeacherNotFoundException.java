724936368fb71fb35651b39006dc9e0a95d7957b
Lee Donghyun <d@donghyun.cc>
2026-02-06 10:24:36 +0900

    fix(#49): Exception 생성자 접근 제어자를 public으로 변경
    StudentNotFoundException, TeacherNotFoundException 생성자를 private에서 public으로 변경하여 외부에서 인스턴스 생성 가능하도록 수정

--- src/main/java/team/weero/app/application/exception/teacher/TeacherNotFoundException.java
+++ src/main/java/team/weero/app/application/exception/teacher/TeacherNotFoundException.java
@@ -1,13 +1,13 @@
 package team.weero.app.application.exception.teacher;
 
-  import team.weero.app.application.exception.teacher.error.TeacherErrorCode;
-  import team.weero.app.global.error.exception.WeeRoException;
+import team.weero.app.application.exception.teacher.error.TeacherErrorCode;
+import team.weero.app.global.error.exception.WeeRoException;
 
-  public class TeacherNotFoundException extends WeeRoException {
+public class TeacherNotFoundException extends WeeRoException {
 
-    public static final TeacherNotFoundException INSTANCE = new TeacherNotFoundException();
+  public static final TeacherNotFoundException INSTANCE = new TeacherNotFoundException();
 
-    private TeacherNotFoundException() {
-      super(TeacherErrorCode.TEACHER_NOT_FOUND);
-    }
+  public TeacherNotFoundException() {
+    super(TeacherErrorCode.TEACHER_NOT_FOUND);
   }
+}
