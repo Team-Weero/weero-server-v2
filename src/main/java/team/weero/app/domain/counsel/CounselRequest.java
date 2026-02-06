65bb0409aee59e32609cf63d23667e4250cb650b
Lee Donghyun <d@donghyun.cc>
2026-02-06 10:24:17 +0900

    refactor(#49): CounselRequest 도메인 모델에서 accessPassword 필드 제거
    본인 글만 조회 가능한 디자인으로 변경됨에 따라 신청글별 비밀번호가 불필요해져 제거

--- src/main/java/team/weero/app/domain/counsel/CounselRequest.java
+++ src/main/java/team/weero/app/domain/counsel/CounselRequest.java
@@ -1,50 +1,48 @@
 package team.weero.app.domain.counsel;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import team.weero.app.domain.counsel.type.Gender;
 import team.weero.app.domain.counsel.type.Status;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Getter
 @Builder
 @AllArgsConstructor
 public class CounselRequest {
 
-    private final UUID id;
-    private final String accessPassword;
-    private final Status status;
-    private final Gender gender;
-    private final boolean hasCounselingExperience;
-    private final String category;
-    private final UUID studentId;
-    private final String studentName;
-    private final UUID teacherId;
-    private final String teacherName;
-    private final LocalDateTime createdAt;
-    private final LocalDateTime updatedAt;
-    private final LocalDateTime deletedTime;
+  private final UUID id;
+  private final Status status;
+  private final Gender gender;
+  private final boolean hasCounselingExperience;
+  private final String category;
+  private final UUID studentId;
+  private final String studentName;
+  private final UUID teacherId;
+  private final String teacherName;
+  private final LocalDateTime createdAt;
+  private final LocalDateTime updatedAt;
+  private final LocalDateTime deletedTime;
 
-    public boolean isDeleted() {
-        return deletedTime != null;
-    }
+  public boolean isDeleted() {
+    return deletedTime != null;
+  }
 
-    public boolean isPending() {
-        return status == Status.PENDING;
-    }
+  public boolean isPending() {
+    return status == Status.PENDING;
+  }
 
-    public boolean isInProgress() {
-        return status == Status.IN_PROGRESS;
-    }
+  public boolean isInProgress() {
+    return status == Status.IN_PROGRESS;
+  }
 
-    public boolean isCompleted() {
-        return status == Status.COMPLETED;
-    }
+  public boolean isCompleted() {
+    return status == Status.COMPLETED;
+  }
 
-    public boolean isCancelled() {
-        return status == Status.CANCELLED;
-    }
+  public boolean isCancelled() {
+    return status == Status.CANCELLED;
+  }
 }
