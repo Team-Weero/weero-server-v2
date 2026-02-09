e2167b658fbda93fc34157d25d197b4b15dea41e
Lee Donghyun <d@donghyun.cc>
2026-02-09 20:08:54 +0900

    fix: Command 객체에서 불필요한 @Schema 어노테이션 제거

--- src/main/java/team/weero/app/adapter/in/web/auth/dto/request/SignInCommand.java
+++ src/main/java/team/weero/app/adapter/in/web/auth/dto/request/SignInCommand.java
@@ -1,8 +1,5 @@
 package team.weero.app.adapter.in.web.auth.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "로그인 커맨드")
 public record SignInCommand(
-    @Schema(description = "이메일", example = "user@example.com") String email,
-    @Schema(description = "비밀번호", example = "password123") String password) {}
+    String email,
+    String password) {}
