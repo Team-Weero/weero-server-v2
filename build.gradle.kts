747a8a148a7713b81548efa68e9ba0b2f743b673
Lee Donghyun <d@donghyun.cc>
2026-02-09 20:07:42 +0900

    fix: springdoc-openapi 버전을 2.8.15로 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
 	implementation("org.apache.kafka:kafka-streams")
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
