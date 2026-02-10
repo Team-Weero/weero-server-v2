e6774239553895fcafaff5314d76132780a50aaf
Lee Donghyun <d@donghyun.cc>
2026-02-10 13:49:26 +0900

    feat: Swagger UI에 HTTPS/HTTP 서버 선택 옵션 추가

--- src/main/java/team/weero/app/global/config/SwaggerConfig.java
+++ src/main/java/team/weero/app/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package team.weero.app.global.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,8 @@ public class SwaggerConfig {
   @Bean
   public OpenAPI openAPI() {
     return new OpenAPI()
+        .addServersItem(new Server().url("https://").description("HTTPS Server"))
+        .addServersItem(new Server().url("http://").description("HTTP Server"))
         .info(new Info().title("Weero Server API").version("2.0").description("Weero 서버 API 문서"))
         .components(
             new io.swagger.v3.oas.models.Components()
