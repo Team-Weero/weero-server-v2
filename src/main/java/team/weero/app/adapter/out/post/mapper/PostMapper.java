8094a0a2ffd33b3fb12fb383dd4da680751f1b5d
박지민 <pjmlove2008@gmail.com>
2026-02-11 11:07:25 +0900

    fix(#47): toEntity 시 누락된 필드 추가

--- src/main/java/team/weero/app/adapter/out/post/mapper/PostMapper.java
+++ src/main/java/team/weero/app/adapter/out/post/mapper/PostMapper.java
@@ -10,25 +10,28 @@ public class PostMapper {
 
   public Post toDomain(PostJpaEntity entity) {
     return Post.builder()
-        .id(entity.getId())
-        .title(entity.getTitle())
-        .content(entity.getContent())
-        .studentId(entity.getStudent().getId())
-        .nickName(entity.getStudent().getNickname())
-        .viewCount(entity.getViewCount())
-        .createdAt(entity.getCreatedAt())
-        .updatedAt(entity.getUpdatedAt())
-        .deletedAt(entity.getDeletedAt())
-        .build();
+            .id(entity.getId())
+            .title(entity.getTitle())
+            .content(entity.getContent())
+            .studentId(entity.getStudent().getId())
+            .nickName(entity.getStudent().getNickname())
+            .viewCount(entity.getViewCount())
+            .createdAt(entity.getCreatedAt())
+            .updatedAt(entity.getUpdatedAt())
+            .deletedAt(entity.getDeletedAt())
+            .build();
   }
 
   public static PostJpaEntity toEntity(Post post, StudentJpaEntity student) {
     return PostJpaEntity.builder()
-        .id(post.getId())
-        .title(post.getTitle())
-        .content(post.getContent())
-        .student(student)
-        .viewCount(post.getViewCount())
-        .build();
+            .id(post.getId())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .student(student)
+            .viewCount(post.getViewCount())
+            .createdAt(post.getCreatedAt())
+            .updatedAt(post.getUpdatedAt())
+            .deletedAt(post.getDeletedAt())
+            .build();
   }
 }
