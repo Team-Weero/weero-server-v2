5e61a946a564d56c5cd87f8477b4568af8d3d860
박지민 <pjmlove2008@gmail.com>
2026-02-12 13:51:08 +0900

    fix(#54): 잘못된 임포트 수정

--- src/main/java/team/weero/app/global/security/config/SecurityConfig.java
+++ src/main/java/team/weero/app/global/security/config/SecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import team.weero.app.global.security.handler.CustomAccessDeniedHandler;
-import team.weero.app.global.security.handler.CustomAuthenticationEntryPoint;
+import team.weero.app.global.error.handler.CustomAccessDeniedHandler;
+import team.weero.app.global.error.handler.CustomAuthenticationEntryPoint;
 import team.weero.app.global.security.jwt.JwtAuthenticationFilter;
 
 @Configuration
