523c9383644a628095f912ace1beaaa19f5e8aa3
박지민 <pjmlove2008@gmail.com>
2026-02-20 10:55:40 +0900

    feat(#58): 불필요한 필드 제거 및 Swagger 문서화 작업

--- src/main/java/team/weero/app/adapter/in/web/chat/dto/request/ChatMessageRequest.java
+++ src/main/java/team/weero/app/adapter/in/web/chat/dto/request/ChatMessageRequest.java
@@ -1,5 +1,7 @@
 package team.weero.app.adapter.in.web.chat.dto.request;
 
-import java.util.UUID;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record ChatMessageRequest(UUID senderId, String text) {}
+@Schema(description = "채팅 메시지 요청")
+public record ChatMessageRequest(
+        @Schema(description = "메시지 내용", example = "안녕하세요") String text) {}
