2c714c8cb20a826d196907c333249f34c769b83a
박지민 <pjmlove2008@gmail.com>
2026-02-22 09:34:00 +0900

    fix(#58): ChatMessage 조회 시 LazyInitializationException 발생 가능 문제 수정

--- src/main/java/team/weero/app/adapter/out/chat/repository/ChatMessageRepository.java
+++ src/main/java/team/weero/app/adapter/out/chat/repository/ChatMessageRepository.java
@@ -3,11 +3,14 @@ package team.weero.app.adapter.out.chat.repository;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.weero.app.adapter.out.chat.entity.ChatMessageJpaEntity;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessageJpaEntity, UUID> {
+
+  @EntityGraph(attributePaths = {"user", "chatRoom"})
   Page<ChatMessageJpaEntity> findByChatRoom_Id(UUID chatRoomId, Pageable pageable);
 }
