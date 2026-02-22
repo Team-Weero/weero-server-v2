af3f803a7b51d6a8b29fa11ba3c283fae726afce
박지민 <pjmlove2008@gmail.com>
2026-02-22 09:28:34 +0900

    feat(#58): 담당 선생님 검증 로직 추가

--- src/main/java/team/weero/app/application/service/counsel/CloseCounselService.java
+++ src/main/java/team/weero/app/application/service/counsel/CloseCounselService.java
@@ -3,11 +3,13 @@ package team.weero.app.application.service.counsel;
 import java.io.IOException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.weero.app.application.exception.counsel.CounselRequestNotFoundException;
 import team.weero.app.application.exception.teacher.TeacherNotFoundException;
 import team.weero.app.application.port.in.counsel.CloseCounselUseCase;
+import team.weero.app.application.port.in.teacher.dto.response.TeacherInfo;
 import team.weero.app.application.port.out.chat.LoadChatRoomPort;
 import team.weero.app.application.port.out.chat.NotifyCounselClosedPort;
 import team.weero.app.application.port.out.counsel.LoadCounselRequestPort;
@@ -29,10 +31,15 @@ public class CloseCounselService implements CloseCounselUseCase {
 
   @Override
   public void execute(UUID chatRoomId, UUID userId) {
-    loadTeacherPort.loadByUserId(userId).orElseThrow(TeacherNotFoundException::new);
+    TeacherInfo teacher =
+        loadTeacherPort.loadByUserId(userId).orElseThrow(TeacherNotFoundException::new);
 
     ChatRoom chatRoom =
         loadChatRoomPort.loadById(chatRoomId).orElseThrow(CounselRequestNotFoundException::new);
+
+    if (!chatRoom.getTeacherId().equals(teacher.id())) {
+      throw new AccessDeniedException("해당 상담 종료 권한이 없습니다.");
+    }
 
     CounselRequest counselRequest =
         loadCounselRequestPort
